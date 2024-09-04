7d2ad4f8acdbb10fc23f3c9ac2876f0cc7b22a2a
Sweety <sweety.x.ext@wsa.com>
2024-09-04 18:52:28 +0530

    first commit

---
+++ aspcore-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NameForm from './NameForm';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Name Concatenation</h1>
+            <NameForm />
+        </div>
+    );
+}
+
+export default App;
+
