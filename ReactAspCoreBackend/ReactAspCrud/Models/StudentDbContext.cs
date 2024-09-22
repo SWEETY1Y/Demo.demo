3163d508d977fd99e5d114dcdb22a9b231558c4f
SWEETY1Y <138777185+SWEETY1Y@users.noreply.github.com>
2024-09-22 17:08:35 +0530

    Update StudentDbContext.cs

--- ReactAspCoreBackend/ReactAspCrud/Models/StudentDbContext.cs
+++ ReactAspCoreBackend/ReactAspCrud/Models/StudentDbContext.cs
@@ -9,9 +9,9 @@ namespace ReactAspCrud.Models
 
         public DbSet<NameModels> NameModel  { get; set; }
       
-         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-          optionsBuilder.UseSqlServer("Data Source=dinm5cg3343d3l;Initial Catalog=Name;User= sa; password=Password@123; Trust Server Certificate=True");
-        }
+      //   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       // {
+         // optionsBuilder.UseSqlServer("Data Source=dinm5cg3343d3l;Initial Catalog=Name;User= sa; password=Password@123; Trust Server Certificate=True");
+        //}
     }
 }
